21d778e2cfb3627b6545f2a0bd750920dee72e8d
Oğuz Demirel <oguz@endcozum.com>
2022-11-04 11:45:44 +0300

    ProductHandler INSERT için Query,Handler yazıldı.

---
+++ Api/WebAPI/Manager/ProductEvent/Insert/InsertProductCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Core.Results;
+using DataAccess.Repo.UOW;
+using Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPI.Manager.ProductEvent.Insert
+{
+    public partial class InsertProductCommandHandler : IRequestHandler<InsertProductCommandQuery, SuccessDataResult<Product>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public InsertProductCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<SuccessDataResult<Product>> Handle(InsertProductCommandQuery request, CancellationToken cancellationToken)
+        {
+            Product _Product = new()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                CategoryID = request.CategoryID
+            };
+            return await Task.FromResult(await _unitOfWork.ProductRepository.Insert(_Product));
+        }
+    }
+}
---
+++ Api/WebAPI/Manager/ProductEvent/Insert/InsertProductCommandQuery.cs
@@ -0,0 +1,14 @@
+﻿using Core.Results;
+using Entities;
+using MediatR;
+using System;
+
+namespace WebAPI.Manager.ProductEvent.Insert
+{
+    public partial class InsertProductCommandQuery : IRequest<SuccessDataResult<Product>>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid CategoryID { get; set; }
+    }
+}
