5916ec404f25fa0d42395c63190578a0a4956119
Oğuz Demirel <oguz@endcozum.com>
2022-11-04 11:04:51 +0300

    AppDbContext sınıfı hazırlandı

---
+++ Core/DataAccess/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) => base.OnModelCreating(modelBuilder);
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
