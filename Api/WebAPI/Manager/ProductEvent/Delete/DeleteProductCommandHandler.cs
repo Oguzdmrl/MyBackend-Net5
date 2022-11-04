a327145188fb028154ee6699b8427f0bdcac4d6c
Oğuz Demirel <oguz@endcozum.com>
2022-11-04 11:45:37 +0300

    ProductHandler DELETE için Query,Handler yazıldı.

---
+++ Api/WebAPI/Manager/ProductEvent/Delete/DeleteProductCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Core.Results;
+using DataAccess.Repo.UOW;
+using Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPI.Manager.ProductEvent.Delete
+{
+    public partial class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommandQuery, SuccessDataResult<Product>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DeleteProductCommandHandler(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+        public async Task<SuccessDataResult<Product>> Handle(DeleteProductCommandQuery request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(await _unitOfWork.ProductRepository.Delete(new Product() { ID = request.ProductID }));
+        }
+    }
+}
---
+++ Api/WebAPI/Manager/ProductEvent/Delete/DeleteProductCommandQuery.cs
@@ -0,0 +1,12 @@
+﻿using Core.Results;
+using Entities;
+using MediatR;
+using System;
+
+namespace WebAPI.Manager.ProductEvent.Delete
+{
+    public partial class DeleteProductCommandQuery : IRequest<SuccessDataResult<Product>>
+    {
+        public Guid ProductID { get; set; }
+    }
+}
