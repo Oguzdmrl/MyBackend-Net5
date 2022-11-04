e7079db76920e7442151a62d56adb7a5d4a000b2
Oğuz Demirel <oguz@endcozum.com>
2022-11-04 11:27:17 +0300

    Product & Category Repo eklendi

---
+++ ApiCore/DataAccess/Repo/EfRepo/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Repo.Base;
+using Entities;
+
+namespace DataAccess.Repo.EfRepo
+{
+    public interface ICategoryRepository : IRepository<Category> { }
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDBContext dBContext) : base(dBContext) { }
+    }
+}
---
+++ ApiCore/DataAccess/Repo/EfRepo/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Repo.Base;
+using Entities;
+
+namespace DataAccess.Repo.EfRepo
+{
+    public interface IProductRepository : IRepository<Product> { }
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDBContext dBContext) : base(dBContext) { }
+    }
+}
