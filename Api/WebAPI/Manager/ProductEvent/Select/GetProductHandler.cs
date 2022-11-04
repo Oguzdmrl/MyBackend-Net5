0e684a7cbe0835419311f1936ce5101fa15970ba
Oğuz Demirel <oguz@endcozum.com>
2022-11-04 11:45:29 +0300

    ProductHandler SELECT için Query,Handler yazıldı.

---
+++ Api/WebAPI/Manager/ProductEvent/Select/GetProductHandler.cs
@@ -0,0 +1,29 @@
+﻿using Core.Results;
+using DataAccess.Repo.UOW;
+using Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPI.Manager.ProductEvent.Select
+{
+    public partial class GetProductHandler : IRequestHandler<GetProductQuery, SuccessDataResult<Product>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetProductHandler(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<SuccessDataResult<Product>> Handle(GetProductQuery request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(_unitOfWork.ProductRepository.GetAllInculude(x => x.Category).Result);
+        }
+    }
+    public partial class GetProductIDHandler : IRequestHandler<GetProductIDQuery, SuccessDataResult<Product>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetProductIDHandler(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+        public async Task<SuccessDataResult<Product>> Handle(GetProductIDQuery request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(_unitOfWork.ProductRepository.GetAll(x => x.ID == request.ProductID).Result);
+        }
+    }
+}
---
+++ Api/WebAPI/Manager/ProductEvent/Select/GetProductQuery.cs
@@ -0,0 +1,15 @@
+﻿using Core.Results;
+using Entities;
+using MediatR;
+using System;
+
+namespace WebAPI.Manager.ProductEvent.Select
+{
+    public partial class GetProductQuery : IRequest<SuccessDataResult<Product>>
+    {
+    }
+    public partial class GetProductIDQuery : IRequest<SuccessDataResult<Product>>
+    {
+        public Guid ProductID { get; set; }
+    }
+}
