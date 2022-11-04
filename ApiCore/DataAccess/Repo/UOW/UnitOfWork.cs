66e6b9098001db2a2b8f96ec33e63580e91089bf
Oğuz Demirel <oguz@endcozum.com>
2022-11-04 11:27:41 +0300

    UnitOfWork Kullanıma hazırlandı

---
+++ ApiCore/DataAccess/Repo/UOW/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Repo.EfRepo;
+using System;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repo.UOW
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly AppDBContext _context;
+
+        public IProductRepository ProductRepository { get; private set; }
+
+        public UnitOfWork(AppDBContext context)
+        {
+            _context = context;
+            ProductRepository = new ProductRepository(context);
+        }
+        public async Task CompleteAsync() => await _context.SaveChangesAsync();
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
