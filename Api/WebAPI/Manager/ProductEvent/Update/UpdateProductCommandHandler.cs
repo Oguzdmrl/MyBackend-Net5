70ef85855afa841b1de734d972a6ab6dc8f684b0
Oğuz Demirel <oguz@endcozum.com>
2022-11-04 11:45:52 +0300

    ProductHandler UPDATE için Query,Handler yazıldı.

---
+++ Api/WebAPI/Manager/ProductEvent/Update/UpdateProductCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Core.Results;
+using DataAccess.Repo.UOW;
+using Entities;
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPI.Manager.ProductEvent.Update
+{
+    public partial class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommandQuery, SuccessDataResult<Product>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UpdateProductCommandHandler(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+        public async Task<SuccessDataResult<Product>> Handle(UpdateProductCommandQuery request, CancellationToken cancellationToken)
+        {
+            var GetModel = await _unitOfWork.ProductRepository.GetAll(x => x.ID == request.ProductID);
+            if (GetModel.ModelCount > 0)
+            {
+                GetModel.ListResponseModel.FirstOrDefault().Name = request.Name;
+                GetModel.ListResponseModel.FirstOrDefault().Description = request.Description;
+                GetModel.ListResponseModel.FirstOrDefault().CategoryID = request.CategoryID;
+            }
+            return await Task.FromResult(await _unitOfWork.ProductRepository.Update(GetModel.ListResponseModel.FirstOrDefault()));
+        }
+    }
+}
---
+++ Api/WebAPI/Manager/ProductEvent/Update/UpdateProductCommandQuery.cs
@@ -0,0 +1,15 @@
+﻿using Core.Results;
+using Entities;
+using MediatR;
+using System;
+
+namespace WebAPI.Manager.ProductEvent.Update
+{
+    public partial class UpdateProductCommandQuery : IRequest<SuccessDataResult<Product>>
+    {
+        public Guid ProductID { get; set; }
+        public Guid CategoryID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
